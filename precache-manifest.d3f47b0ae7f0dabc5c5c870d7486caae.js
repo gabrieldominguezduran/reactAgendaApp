572a6dfdb5417b2f351418b56df121cf272db43a
Gabriel Domínguez Durán <gabrieldominguezduran@gmail.com>
2019-08-21 12:27:25 +0200

    Updates

---
+++ precache-manifest.d3f47b0ae7f0dabc5c5c870d7486caae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc015427f95b35c216da4cc8d7ab8c47",
+    "url": "/reactAgendaApp/index.html"
+  },
+  {
+    "revision": "1277d4ad78c0e8468f6e",
+    "url": "/reactAgendaApp/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "2e53229d69c156dda25b",
+    "url": "/reactAgendaApp/static/css/main.be56f562.chunk.css"
+  },
+  {
+    "revision": "1277d4ad78c0e8468f6e",
+    "url": "/reactAgendaApp/static/js/2.9739a4e9.chunk.js"
+  },
+  {
+    "revision": "2e53229d69c156dda25b",
+    "url": "/reactAgendaApp/static/js/main.e174be18.chunk.js"
+  },
+  {
+    "revision": "0f3025cc24738783cb51",
+    "url": "/reactAgendaApp/static/js/runtime~main.cfa8db4d.js"
+  }
+]);
